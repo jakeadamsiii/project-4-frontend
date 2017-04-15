0c25641a02e066cdd8e1010d0da99420409bfb37
Jake Adams <jadams.html@gmail.com>
2017-04-15 09:42:28 +0100

    bcrypt lesson

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,42 @@
-// app.js is the main JS file which you should define your Angular module
+angular
+  .module('fundraiser', ['satellizer'])
+  .constant('API_URL', 'http://localhost:3000/api')
+  .config(Auth)
+  .controller('LoginCtrl', LoginCtrl);
+
+  Auth.$inject = ['$authProvider', 'API_URL'];
+  function Auth($authProvider, API_URL) {
+    $authProvider.signupUrl = `${API_URL}/register`;
+    $authProvider.loginUrl = `${API_URL}/login`;
+
+    $authProvider.github({
+      clientId: '',
+      url: `${API_URL}/oauth/github`
+    });
+  }
+
+LoginCtrl.$inject = ['$auth'];
+function LoginCtrl($auth) {
+  const vm = this;
+
+  function register() {
+    $auth.signup(vm.user)
+      .then(user => console.log(user));
+  }
+
+  vm.register = register;
+
+  function login() {
+    $auth.login(vm.credentials)
+      .then(user => console.log(user));
+  }
+
+  vm.login = login;
+
+  function authenticate(provider) {
+  $auth.authenticate(provider)
+    .then(user => console.log(user));
+}
+
+vm.authenticate = authenticate;
+}
